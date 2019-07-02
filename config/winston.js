d3be174c0ddd059dd23fd64b41e719d11439de13
Mohamed-Mayallo <reckoning58@gmail.com>
2019-07-02 23:54:47 +0200

    Logging with Winston, Morgan, MongoDB and external file

---
+++ config/winston.js
@@ -0,0 +1,49 @@
+var appRoot = require('app-root-path');
+var winston = require('winston');
+require('winston-mongodb');
+
+var winstonMongodbOptions = {
+  db: 'mongodb://127.0.0.1:27017/logger',
+  options: { useNewUrlParser: true },
+  collection: 'logs',
+  storeHost: true
+};
+
+// define the custom settings for each transport (file, console)
+var options = {
+  file: {
+    level: 'info',
+    filename: `${appRoot}/logs/app.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: true
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true
+  }
+};
+
+// instantiate a new Winston Logger with the settings defined above
+var logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console),
+    new winston.transports.MongoDB(winstonMongodbOptions)
+  ],
+  exitOnError: false // do not exit on handled exceptions
+});
+
+// create a stream object with a 'write' function that will be used by `morgan`
+logger.stream = {
+  write: function(message, encoding) {
+    // use the 'info' log level so the output will be picked up by both transports (file and console)
+    logger.info(message);
+  }
+};
+
+module.exports = logger;
